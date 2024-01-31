ab99c1c993948f4bdd741ef9d41b3d6a6520759e
Brody P. Thompson <f004grg@babylon1.thayer.dartmouth.edu>
2024-01-30 20:11:36 -0500

    pseudo code added for all of the syscall section

--- src/syscall_handlers/input_output.c
+++ src/syscall_handlers/input_output.c
@@ -1,10 +1,29 @@
 #include "../../../yalnix_framework/include/yalnix.h"
 #include "../../../yalnix_framework/include/ykernel.h"
 
-int kernelTtyRead(int tty_id, void* buf, int len) {
+
+
+int KernelTtyRead(int tty_id, void* buf, int len) {
+  // read input from terminal, set it to buf
+  // can only set the max number of bytes <= len from tty_id
+  // if there is enough bytes readily availible, set and return
+  // else wait until there is a line of input ready to be returned
+  //    when there is:
+  //      if tty_id > len, then we save the first len bytes, and save remaining bytes to kernel for next read
+  //      this should probably be called by this process if this is the case, but not necessary
+  //      returns the number of bytes actually copied into the buffer
+  //  on any error, return ERROR
   return 0;
 }
 
-int kernelTyWrite(int tty_id, void* buf, int len) {
+int KernelTyWrite(int tty_id, void* buf, int len) {
+  // len is the length of the buffer
+  // process blocked until all bytes are writted to tty_id
+
+  // try to write the bytes to tty_id, if they exceed a limit, do the first amount, then wait for the second amount to be written
+  // on success return the number of bytes written
+  // on any error return ERROR
+
+  // note it appears that the provided function Tty_Printf uses this function, so probably going to want to make sure this is working from a user standpoint
   return 0;
 }
--- src/syscall_handlers/synchronization.c
+++ src/syscall_handlers/synchronization.c
@@ -1,34 +1,116 @@
 #include "../../../yalnix_framework/include/yalnix.h"
 #include "../../../yalnix_framework/include/ykernel.h"
 
-int kernelLockInit(int* lock_ipd) {
+int KernelLockInit(int* lock_ipd) {
+  /* 
+  * STRUCT Lock
+  * int owner
+  * int id
+  * Queue- lock queue for lock to go next
+  * */
+  // create lock
+  // initialize current owner if applicable
+  // store lock in lock_ipd
+  // return upon success, toss error if ERROR such as lock_ipd not existing
+
   return 0;
 }
 
-int kernelAcquire(int lock_id) {
+int KernelAcquire(int lock_id) {
+  // use lock_id to locate lock in memory
+  // check if there is a current owner of the lock
+  // if there is, append user to the queue
+  // if not, give the lock directly to the caller and change the owner var in the lock
+  // return ERROR if problems arise while this is happening
   return 0;
 }
 
-int kernelRelease(int lock_id) {
+int KernelRelease(int lock_id) {
+  // verify that the lock exists
+  // find the lock
+  // if the current process is the owner of the lock
+  //    then release the lock - remove owner/clear var from struct
+  // 
+  // check if there are new owners in the queue
+  // give the lock to the next owner
+  // return ERROR if any of the above checks/attempts fail
+
   return 0;
 }
 
-int kernelCvarInit(int* cvar_ipd) {
+int KernelCvarInit(int* cvar_ipd) {
+  /* 
+  * STRUCT cvar
+  * int owner
+  * int id
+  * Queue- lock queue for lock to go next
+  * */
+  // find the current process in the pcb
+  // find a cvar number that has not been used yet (similar to locks)
+  // alloc mem and set var to pointer in param.... cvar_ipd
+  // set current process to owner
+  // set id to found id above
+  // return if all of the above successful, throw ERROR upon failures
   return 0;
 }
 
-int kernelCvarSignal(int cvar_ip) {
+int KernelCvarSignal(int cvar_ip) {
+  // locate cvar based on the cvar_ip
+  // if its invalid, throw error
+  // find current process
+  // check if there are other processes in the queue
+  // if there are, take the next one out of the queue
+  //  make the next one the current owner, and let that process resume from its wait
+
+  // if all sucessful return, if there were no processes waiting, do nothing
+
+  // throw ERROR where necessary during checks
   return 0;
 }
 
-int kernelCvarBroadcast(int cvar_id) {
+int KernelCvarBroadcast(int cvar_id) {
+  // locate cvar based on the cvar_ip
+  // if its invalid, throw error
+  // find current process
+  // check if there are other processes in the queue
+  // then free all of those processes, from their respective pcbs in memory
+  // return
+
+  //throw ERROR where necessary during checks
+
+  // note this is pretty much the same as signal, except instead of removing one from the queue, we are dequeueing the entire queue.
+  // this means there will need to be more code handling in the surrounding code on the wait.
   return 0;
 }
 
-int kernelCvarWait(int cvar_id, int lock_id) {
+int KernelCvarWait(int cvar_id, int lock_id) {
+  // cvar is the id of the svar that the process is waiting on
+  // lock_id is the lock that its waiting on
+
+  // verify cvar exists
+  // verify lock exists
+
+  // release the lock- so that other processes can use while its waiting
+
+  // make the process wait and enqueue it in the cvar's queue
+
+  // once the var is free aquire the lock and return
+
+  // throw ERROR where necessary during the checks
   return 0;
 }
 
-int kernelReclaim(int id) {
+int KernelReclaim(int id) {
+  // check if its a Lock, Cvar, or a Pipe
+  // locate each one respectively
+  // verify that they exist
+  // Make sure that there are no processes waiting on the lock/cvar/pipe, throw ERROR if not
+  // free the lock/cvar/pipe
+  // dealloc the mem for it
+  // return respectively
+
+  // throw ERROR on checks where necesarry
+  // the above pseudo code is for one, this would in theory would need to be repeated and changed slightly for each cvar/pipe/lock
+
   return 0;
 }
