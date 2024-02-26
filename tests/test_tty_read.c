81145fb68b445805c120326e284cc967657a5cba
sidHathi <sidhathi20@gmail.com>
2024-02-26 00:33:24 -0500

    made the tty test slightly harder

--- tests/test_tty_read.c
+++ tests/test_tty_read.c
@@ -8,4 +8,13 @@ main(int argc, char** argv)
   char* input = malloc(MAX_INPUT * sizeof(char));
   TtyRead(0, input, MAX_INPUT);
   TtyPrintf(0, "input: %s\n", input);
+
+  TtyPrintf(0, "another line\n");
+  TtyRead(0, input, MAX_INPUT);
+  TtyRead(0, input, MAX_INPUT);
+  TtyPrintf(0, "input: %s\n", input);
+
+  TtyPrintf(2, "Let's put something on terminal two\n");
+  TtyRead(2, input, MAX_INPUT);
+  TtyPrintf(1, "Got something on terminal 2\n");
 }
