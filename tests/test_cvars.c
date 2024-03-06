63639edfd7a0e8fdd660a992819b0acabb389633
ashervogel17 <ashervogel17@gmail.com>
2024-03-06 12:30:08 -0500

    ready for final submission

--- tests/test_cvars.c
+++ tests/test_cvars.c
@@ -1,70 +1,32 @@
 #include <yuser.h>
 
-
-typedef struct my_struct {
-  int cookies;
-  int lock_id;
-  int cvar_id;
-} my_struct_t;
-
-int main(int argc, char* argv[]) {
-  my_struct_t* cookie_jar = (my_struct_t*) malloc(sizeof(my_struct_t));
-  cookie_jar->cookies = 0;
-  LockInit(&cookie_jar->lock_id);
-  if (CvarInit(&cookie_jar->cvar_id) == ERROR) {
-    TracePrintf(1, "Couldn't create cvar :( \n");
-    Exit(-1);
-  }
-
-  TracePrintf(1, "Initially, there are %d cookies in the cookie jar.\n", cookie_jar->cookies);
-
-  int rc = Fork();
-
+int main() {
+  TracePrintf(1, "Welcome to cvar test\n");
+  int lock_id, cvar_id, rc;
+  LockInit(&lock_id);
+  CvarInit(&cvar_id);
+  TracePrintf(1, "%d %d YURR\n", lock_id, cvar_id);
+  TracePrintf(1, "About to fork\n");
+  rc = Fork();
   if (rc == 0) {
-    TracePrintf(1, "I am the child.\n");
-    Pause();
-    while (Acquire(cookie_jar->lock_id) == 0) {
-      TracePrintf(1, "Child has the lock\n");
-      if (cookie_jar->cookies < 5) {
-        TracePrintf(1, "Child got the lock but there are only %d cookies!\n", cookie_jar->cookies);
-        Pause();
-        CvarWait(cookie_jar->cvar_id, cookie_jar->lock_id);
-      }
-      else {
-        break;
-      }
+    Acquire(lock_id);
+    TracePrintf(1, "I am the child and I have the lock! I can't be happy until my parent has had it!\n");
+    int i = 1;
+    while (i < 3) {
+      TracePrintf(1, "Child spinning\n");
+      CvarWait(cvar_id, lock_id);
+      i++;
     }
-    TracePrintf(1, "Yay! Child has the lock and there are enough cookies!\n");
-    TracePrintf(1, "Cookies before: %d\n", *(&cookie_jar->cookies));
-    cookie_jar->cookies = *(&cookie_jar->cookies) - 5;
-    TracePrintf(1, "Cookies after: %d\n", cookie_jar->cookies);
-    Release(cookie_jar->lock_id);
-    CvarSignal(cookie_jar->cvar_id);
+    Release(lock_id);
+    CvarSignal(cvar_id);
     Exit(0);
   }
-
   else {
-    TracePrintf(1, "I am the parent\n");
-    Pause();
-    for (int i = 1; i <= 5; i++) {
-      while (Acquire(cookie_jar->lock_id) == 0) {
-        TracePrintf(1, "Parent has the lock\n");
-        if (cookie_jar->cookies >= 5) {
-          TracePrintf(1, "Parent has already made enough cookies!\n");
-          CvarWait(cookie_jar->cvar_id, cookie_jar->lock_id);
-        }
-        else {
-          break;
-        }
-      }
-      TracePrintf(1, "Parent has the lock. Adding 1 cookie!\n");
-      TracePrintf(1, "Cookies before: %d\n", cookie_jar->cookies);
-      cookie_jar->cookies = cookie_jar->cookies + 1;
-      TracePrintf(1, "Cookies after: %d\n", cookie_jar->cookies);
-      Release(cookie_jar->lock_id);
-      CvarSignal(cookie_jar->cvar_id);
-    }
+    Delay(3);
+    Acquire(lock_id);
+    TracePrintf(1, "I am the parent and I have the lock!\n");
+    Release(lock_id);
+    CvarSignal(cvar_id);
     Wait(NULL);
-    Exit(0);
   }
 }
